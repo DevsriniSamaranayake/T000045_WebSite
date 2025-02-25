2c27d9ff387f76b657107664b552a4b8fb57c4c5
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-02-25 17:22:58 +0530

    Update Component

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -7,8 +7,8 @@ interface SmallCardProps {
   const SmallCard: React.FC<SmallCardProps> = ({ title, description, className }) => {
     return (
       <div className={`flex flex-col items-center transform ${className}`}>
-        <div className="p-6 bg-amber-950 rounded-lg shadow-md w-72 h-64 flex flex-col justify-between">
-          <div className="p-4 bg-amber-200 rounded-md shadow-md text-left w-full h-40 flex items-center">
+        <div className="p-6 bg-amber-950 rounded-lg  w-72 h-64 flex flex-col justify-between">
+          <div className="p-4 bg-amber-100 rounded-md  text-left w-full h-40 flex items-center">
             <p className="text-black font-medium">{description}</p>
           </div>
           <h3 className="text-lg font-bold text-amber-400">{title}</h3>
