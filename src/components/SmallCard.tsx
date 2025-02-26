7e61fed30d5b012c310f8646ee878d867e971750
Devsrini Savidya <savidyasamaranayake@gmail.com>
2025-02-26 10:36:52 +0530

    Update RoadMap

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -7,11 +7,11 @@ interface SmallCardProps {
   const SmallCard: React.FC<SmallCardProps> = ({ title, description, className }) => {
     return (
       <div className={`flex flex-col items-center transform ${className}`}>
-        <div className="p-6 bg-amber-950 rounded-lg  w-72 h-64 flex flex-col justify-between">
-          <div className="p-4 bg-amber-100 rounded-md  text-left w-full h-40 flex items-center">
+        <div className="p-6 bg-[#350301] rounded-lg  md:w-72 md:h-64 sm:w-[400px] sm:h-auto flex flex-col justify-between">
+          <div className="p-4 mb-3 bg-[#FFFBDE] rounded-md text-left w-full h-40 flex items-center">
             <p className="text-black font-medium">{description}</p>
           </div>
-          <h3 className="text-lg font-bold text-amber-400">{title}</h3>
+          <h3 className="text-lg font-bold text-[#FDE63B]">{title}</h3>
         </div>
       </div>
     );
--- src/section/mainSection/roadMap.tsx
+++ src/section/mainSection/roadMap.tsx
@@ -4,69 +4,133 @@ const RoadMap = () => {
   return (
     <div
       id="roadmap"
-      className="container max-w-[2050px] mx-auto min-h-screen bg-cover bg-center bg-no-repeat bg-amber-500 flex flex-col items-center"
+      className="container w-full max-w-[2050px] mx-auto min-h-screen bg-cover bg-center bg-no-repeat bg-[#EE8F1C] flex flex-col items-center px-6"
       style={{ backgroundImage: "url('/RoadMapWallpaper.webp')" }}
     >
-      <h2 className="stroke-yellow text-5xl font-bold text-center mt-12">
-        Road Map  
+      <h2 className="stroke-yellow text-5xl font-bold text-center mt-12 x-6 py-4">
+        Road Map
       </h2>
 
       {/* Roadmap Container */}
-      <div className="flex flex-col md:flex-row items-center mt-16 relative border border-black h-[500px] w-full">
-        
-        {/* Phase 1 */}
-        {/* <div className="absolute left-8"> */}
-        <div className="absolute left-8 top-8 border border-blue-500">
+      <div className="w-full mx-auto flex flex-col items-center justify-center mt-16 px-4 py-8 md:py-16">
+
+        {/* Desktop View*/}
+        <div className="hidden lg:flex w-full relative">
+          {/* Phase 1 */}
+          <div className="absolute left-8 top-8">
+            <SmallCard
+              title="Phase 01"
+              description="Create, audit, & deploy smart contract for secure token launch."
+              className="-rotate-12"
+            />
+          </div>
+
+          {/* Arrow */}
+          <div className="absolute left-[310px] top-[80px]">
+            <img src="/Arrow_1.png" className="w-10 md:w-20 h-10 md:h-20 rotate-90 lg:rotate-0" />
+          </div>
+
+          {/* Phase 2 */}
+          <div className="absolute left-[380px] top-[150px]">
+            <SmallCard
+              title="Phase 02"
+              description="Research, define token utility, ecosystem, and market."
+              className="-rotate-6"
+            />
+          </div>
+
+          {/* Arrow */}
+          <div className="absolute left-[680px] top-[300px]">
+            <img src="/Arrow2.webp" className="w-10 md:w-20 h-10 md:h-20 rotate-90 lg:rotate-0" />
+          </div>
+
+          {/* Phase 3 */}
+          <div className="absolute left-[750px] top-[30px]">
+            <SmallCard
+              title="Phase 03"
+              description="Integrate tokens with decentralized applications, drive adoption."
+              className="-rotate-12"
+            />
+          </div>
+
+          {/* Arrow */}
+          <div className="absolute left-[1050px] top-[100px]">
+            <img src="/Arrow_1.png" className="w-10 md:w-20 h-10 md:h-20 rotate-90 lg:rotate-0" />
+          </div>
+
+          {/* Phase 4 */}
+          <div className="absolute left-[1150px] top-[130px]">
+            <SmallCard
+              title="Phase 04"
+              description="Build partnerships, promote platform, engage community through incentives."
+              className="-rotate-12"
+            />
+          </div>
+        </div>
+
+        {/* Tablet View */}
+        <div className="hidden md:grid lg:hidden grid-cols-1 md:grid-cols-2 gap-8 w-full place-items-center mt-6">
+          {/* Row 1 */}
+          <div className="flex flex-col items-center">
+            <SmallCard
+              title="Phase 01"
+              description="Create, audit, & deploy smart contract for secure token launch."
+            />
+            <img src="/Arrow_1.png" className=" md:hidden w-10 h-10 mt-4" />
+          </div>
+
+          <div className="flex flex-col items-center">
+            <SmallCard
+              title="Phase 02"
+              description="Research, define token utility, ecosystem, and market."
+            />
+          </div>
+
+          {/* Row 2 */}
+          <div className="flex flex-col items-center">
+            <SmallCard
+              title="Phase 03"
+              description="Integrate tokens with decentralized applications, drive adoption."
+            />
+            <img src="/Arrow_1.png" className="md:hidden w-10 h-10 mt-4" />
+          </div>
+
+          <div className="flex flex-col items-center">
+            <SmallCard
+              title="Phase 04"
+              description="Build partnerships, promote platform, engage community through incentives."
+            />
+          </div>
+        </div>
+
+        {/* Mobile View */}
+        <div className="flex flex-col items-start gap-8 md:hidden w-full mt-6 relative">
           <SmallCard
             title="Phase 01"
             description="Create, audit, & deploy smart contract for secure token launch."
-            className="-rotate-12"
+            className="-rotate-6"
           />
-        </div>
+          <img src="/Arrow_Road1.png" className="absolute left-[410px] top-[100px] w-auto h-auto " />
 
-        {/* Arrow */}
-        <div className="absolute left-[310px] top-[80px] border border-red-500  ">
-          <img src="/Arrow_1.png" className="w-10 md:w-20 h-10 md:h-20 rotate-90 md:rotate-0" />
-        </div>
-
-       
-        {/* <div className="relative top-40"> */}
-        <div className="absolute left-[380px] top-[150px] border border-blue-500">
           <SmallCard
             title="Phase 02"
             description="Research, define token utility, ecosystem, and market."
-            className="-rotate-6"
+            className="-rotate-6 left-[100px] relative"
           />
-        </div>
+          <img src="/Arrow_Road2.png" className="absolute left-[60px] top-[430px]" />
 
-        {/* Arrow */}
-        {/* <div className="relative top-36"> */}
-        <div className="absolute left-[680px] top-[300px] border border-red-500">
-          <img src="/Arrow2.webp" className="w-10 md:w-20 h-10 md:h-20 rotate-90 md:rotate-0" />
-        </div>
-
-        {/* Phase 3 */}
-        <div className="absolute left-[750px] top-[30px] border border-blue-500">
           <SmallCard
             title="Phase 03"
             description="Integrate tokens with decentralized applications, drive adoption."
-            className="-rotate-12"
+            className="-rotate-6"
           />
-        </div>
+          <img src="/Arrow_Road1.png" className="absolute top-[650px] left-[410px]" />
 
-        {/* Arrow */}
-        <div className="absolute left-[1050px] border border-red-500">
-          <img src="/Arrow_1.png" className="w-10 md:w-20 h-10 md:h-20 rotate-90 md:rotate-0" />
-        </div>
-
-     
-        {/* <div className="relative top-40"> */}
-        <div className="absolute left-[1150px] top-[130px] border border-blue-500">  
           <SmallCard
             title="Phase 04"
             description="Build partnerships, promote platform, engage community through incentives."
-            className="-rotate-12"
-          />
+            className="-rotate-6 left-[100px] relative"
+          /> 
         </div>
 
       </div>
@@ -75,3 +139,4 @@ const RoadMap = () => {
 };
 
 export default RoadMap;
+
